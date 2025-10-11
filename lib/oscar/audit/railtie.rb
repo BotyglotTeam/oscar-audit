807539f4701bebe7cdcf23d7b60634d712db7c68
Dorian <dorian@botyglot.com>
2025-10-11 17:42:17 +0200

    Remove `Oscar::Audit::Auditable` module and its integration in Railtie
    - Deleted the `Auditable` module, including `audit_log` functionality.
    - Removed the ActiveRecord integration for `Auditable` from the Railtie initializer.

--- lib/oscar/audit/railtie.rb
+++ lib/oscar/audit/railtie.rb
@@ -2,12 +2,6 @@
 module Oscar
   module Audit
     class Railtie < ::Rails::Railtie
-      initializer "oscar.audit.auditable" do
-        ActiveSupport.on_load(:active_record) do
-          require "oscar/audit/auditable"
-          ActiveRecord::Base.include(::Oscar::Audit::Auditable)
-        end
-      end
     end
   end
 end
