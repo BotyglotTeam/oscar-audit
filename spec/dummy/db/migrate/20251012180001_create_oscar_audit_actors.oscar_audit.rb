3bc371dd7c9fadb1c73665b963655d1825233d4a
Dorian <dorian@botyglot.com>
2025-10-12 18:14:10 +0200

    wip

---
+++ spec/dummy/db/migrate/20251012180001_create_oscar_audit_actors.oscar_audit.rb
@@ -0,0 +1,23 @@
+# This migration comes from oscar_audit (originally 20251012180000)
+class CreateOscarAuditActors < ActiveRecord::Migration[8.0]
+  def change
+    create_table :oscar_audit_actors, **table_id_opt do |t|
+      t.integer :type, null: false, default: 0
+      t.string :name, null: false
+      t.timestamps
+    end
+
+    add_index :oscar_audit_actors, :type, name: "idx_oaudit_actors_on_type"
+  end
+
+  private
+  # Use in any migration
+  def host_pk_type
+    Rails.application.config.generators
+         .options.dig(:active_record, :primary_key_type)
+  end
+
+  def table_id_opt
+    (t = host_pk_type) ? { id: t } : {}
+  end
+end
