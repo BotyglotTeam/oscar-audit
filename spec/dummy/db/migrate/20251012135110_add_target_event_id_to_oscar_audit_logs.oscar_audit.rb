8f1f7487e814ab1fc506441faa4f05e6c2b15b58
Dorian <dorian@botyglot.com>
2025-10-12 16:01:34 +0200

    wio

---
+++ spec/dummy/db/migrate/20251012135110_add_target_event_id_to_oscar_audit_logs.oscar_audit.rb
@@ -0,0 +1,9 @@
+# This migration comes from oscar_audit (originally 20251011084601)
+class AddTargetEventIdToOscarAuditLogs < ActiveRecord::Migration[8.0]
+  def change
+    add_column :oscar_audit_logs, :target_event_id, :string, null: true
+    add_index :oscar_audit_logs, :target_event_id, unique: true
+  end
+
+
+end
