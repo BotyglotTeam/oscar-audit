facc5889a8eae3b11b268fb5af96b1c7ab5dd966
dlupu <dorian@botyglot.com>
2025-11-25 00:23:04 +0100

    wip

--- spec/dummy/spec/support/oscar_activities.rb
+++ spec/dummy/spec/support/oscar_activities.rb
@@ -1,15 +1,15 @@
-Oscar::Activities.disable_application_logs!
+Oscar::Activities.disable_application_activities!
 
 RSpec.configure do |config|
 
   config.before :each, type: :feature do
-    Oscar::Activities.enable_application_logs!
+    Oscar::Activities.enable_application_activities!
   end
 
   config.before :each, require_application_logs: true do |example|
     unless example.metadata[:type] == :feature
       # feature test already have the seed loaded (see above)
-      Oscar::Activities.enable_application_logs!
+      Oscar::Activities.enable_application_activities!
     end
   end
 end
