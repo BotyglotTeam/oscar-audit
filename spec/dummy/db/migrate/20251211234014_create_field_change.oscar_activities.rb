c0de257092b04c0494298cb7d94ffffec5f9cec4
dlupu <dorian@botyglot.com>
2025-12-12 00:46:28 +0100

    wip

--- spec/dummy/db/migrate/20251211234014_create_field_change.oscar_activities.rb
+++ spec/dummy/db/migrate/20251211234014_create_field_change.oscar_activities.rb
@@ -1,3 +1,4 @@
+# This migration comes from oscar_activities (originally 20251211231630)
 class CreateFieldChange < ActiveRecord::Migration[8.0]
   def change
     create_table :activities_field_changes,  **table_id_opt  do |t|
